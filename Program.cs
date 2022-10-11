b8b6d5eb8699a812081c23586d7a1a4c9d557f62
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-10-11 23:45:41 +0600

    Work Done

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DataContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DConnection"));
 });
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
